551b4334a707eab063282c84b79e7a4361f40aaf
Hyaguinhogp <hyago.v.a.m@gmail.com>
2023-12-09 12:21:49 -0400

    fix: request user id

--- _core/permissions.py
+++ _core/permissions.py
@@ -10,7 +10,7 @@ class IsAuthenticated(BasePermission):
         try:
             token = request.headers['Authorization'].split(' ')[1]
             decoded_token = jwt.decode(token, 'secret', algorithms='HS256')
-            request.is_superuser = User.objects.get(id=decoded_token['user_id']).is_superuser
+            request.user_id = decoded_token['user_id']
             return True
 
         except KeyError:
@@ -24,7 +24,8 @@ class IsAuthenticated(BasePermission):
         
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        if request.is_superuser:
+        is_superuser = User.objects.get(id=request.user_id).is_superuser
+        if is_superuser:
             return True
         
         raise AuthenticationFailed('Sem autorização')
