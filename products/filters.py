0a1fa22fbae3466b784ebda559d7a96efe8d137d
wandryell <wandryell@gmail.com>
2023-12-18 21:19:02 -0400

    feat: filter products

---
+++ products/filters.py
@@ -0,0 +1,10 @@
+from django_filters import rest_framework as filters
+
+from .models import Products
+
+
+class ProductsFilter(filters.FilterSet):
+    
+    class Meta:
+        model = Products
+        fields = '__all__'
