74cbef03769fe66566ad2bdd37e1b2c492b8f543
wandryell <wandryell@gmail.com>
2023-12-19 11:39:30 -0400

    feat: products filter

--- products/views.py
+++ products/views.py
@@ -1,17 +1,21 @@
 from rest_framework import generics
+from django_filters import rest_framework as filters
 
 from _core.permissions import IsAuthenticated
 from .models import Products
-from .serializers import ProductsSerializers
+from .serializers import ProductsSerializer
+from .filters import ProductsFilter
 
 
-class ProductsListCreate(generics.ListAPIView):
+class ProductsListCreate(generics.ListCreateAPIView):
     queryset = Products.objects.all()
-    serializer_class = ProductsSerializers
+    serializer_class = ProductsSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ProductsFilter
     permission_classes = [IsAuthenticated]
 
     
 class ProductsDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Products.objects.all()
-    serializer_class = ProductsSerializers
+    serializer_class = ProductsSerializer
     permission_classes = [IsAuthenticated]
